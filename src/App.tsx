531fd23b889d85e1add4055919ad18bfcc2188ae
chimba <2chimba@mail.ru>
2025-10-08 11:59:28 +0800

    Add some trash

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite1 + React2</h1>
+      <h1>Vite12 + React23</h1>
+
+
       <div className="card">
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+
         </button>
+
         <p>
+
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
